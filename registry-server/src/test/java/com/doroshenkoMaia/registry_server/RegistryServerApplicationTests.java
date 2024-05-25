e053cccc16b254011c0bea71744c525bb35650fe
Maia Doroshenko <maiadoroshenko@gmail.com>
2024-05-25 16:31:48 -0300

    Configura Eureka Serever

---
+++ registry-server/src/test/java/com/doroshenkoMaia/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doroshenkoMaia.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
