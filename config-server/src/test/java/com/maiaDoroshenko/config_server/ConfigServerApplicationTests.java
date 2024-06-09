349c2ff4c0e91dccc18a491f09837e6b52789412
Maia Doroshenko <maiadoroshenko@gmail.com>
2024-06-09 16:37:32 -0300

    crea repo git para guardar las configuraciones, crea token de acceso y configura variable de enetorno

---
+++ config-server/src/test/java/com/maiaDoroshenko/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiaDoroshenko.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
